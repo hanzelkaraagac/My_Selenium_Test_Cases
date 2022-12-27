d288bbc15cc71d0010bee96680a3e65a8ce57a76
hanzelkaraagac <hanzelgamess@gmail.com>
2022-12-28 01:37:12 +0300

    2022-12-28

--- src/test/java/testcases02/Test_Case01.java
+++ src/test/java/testcases02/Test_Case01.java
@@ -1,13 +1,50 @@
 package testcases02;
 
-public class Test_Case01 {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
+import org.openqa.selenium.ScreenOrientation;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-    //-"sahibinden.com" adresine gidelim
-    //-bu web adresinin sayfa basligini(title) ve adres(url)ini yazdıralım
-    //-title ve url'nin "Oto" kelimesinin icerip icermedigini kontrol edelim
-    //-Ardindan "trendyol.com" adresine gidelim
-    //-bu adresin basligini alalim ve "Sitesi" kelismesini icerip icermedigini kontrol edelim
-    //-Bi onceki web sayfamiza geri donelim
-    //-sayfayi yenileyelim
-    //-Daha sonra trendyol sayfamiza tekrar donelim ve sayfayi kapatalim
+import java.time.Duration;
+
+public class Test_Case01 {
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+
+        //-"sahibinden.com" adresine gidelim
+        driver.get("https://sahibinden.com");
+        Thread.sleep(3000);
+        //-bu web adresinin sayfa basligini(title) ve adres(url)ini yazdıralım
+        String sayfaTitle = driver.getTitle();
+        String sayfaUrl = driver.getCurrentUrl();
+        System.out.println("sayfaTitle = " + sayfaTitle);
+        System.out.println("sayfaUrl = " + sayfaUrl);
+
+        //-title ve url'nin "Oto" kelimesinin icerip icermedigini kontrol edelim
+        Assert.assertFalse(sayfaTitle.contains("Oto") && sayfaUrl.contains("Oto"));
+
+        //-Ardindan "trendyol.com" adresine gidelim
+        driver.get("https://trendyol.com");
+
+        //-bu adresin basligini alalim ve "Sitesi" kelismesini icerip icermedigini kontrol edelim
+        String trendyolTitle = driver.getTitle();
+        Assert.assertTrue(trendyolTitle.contains("Sitesi"));
+        System.out.println(trendyolTitle);
+
+        //-Bi onceki web sayfamiza geri donelim
+        driver.navigate().back();
+
+        //-sayfayi yenileyelim
+        driver.navigate().refresh();
+
+        //-Daha sonra trendyol sayfamiza tekrar donelim ve sayfayi kapatalim
+        driver.navigate().forward();
+        driver.close();
+    }
 }
